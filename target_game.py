f3b75eca10be36c28d1e19cb296a4d4576f55b44
codefloww <kryvpaul@gmail.com>
2021-10-28 18:03:11 +0300

    added documentaion and debugged

--- target_game.py
+++ target_game.py
@@ -3,9 +3,11 @@ import random
 
 
 def generate_grid() -> list[list[str]]:
-    """
-    Generates list of lists of letters - i.e. grid for the game.
+    """Generates list of lists of letters - i.e. grid for the game.
     e.g. [['I', 'G', 'E'], ['P', 'I', 'S'], ['W', 'M', 'G']]
+
+    Returns:
+        list[list[str]]: playing grid of letters
     """
     alphabet = string.ascii_uppercase
     letters_grid = [
@@ -23,7 +25,12 @@ def generate_grid() -> list[list[str]]:
     return letters_grid
 
 
-def display_grid(letters_grid: list[list[str]]):
+def display_grid(letters_grid: list[list[str]]) -> None:
+    """Displays playing grid of letters
+
+    Args:
+        letters_grid (list[list[str]]): grid of letters
+    """
     grid = ""
     for i in range(len(letters_grid)):
         grid += " ".join(letters_grid[i]) + "\n"
@@ -31,6 +38,14 @@ def display_grid(letters_grid: list[list[str]]):
 
 
 def count_appearances(word: str) -> list[tuple]:
+    """Checks how many appearance of each letter in word
+
+    Args:
+        word (str): word to check
+
+    Returns:
+        list[tuple]: list of tuples('letter', appearances)
+    """
     counter_letter = []
     word = word.lower()
     for i in range(len(word)):
@@ -41,7 +56,16 @@ def count_appearances(word: str) -> list[tuple]:
     return counter_letter
 
 
-def check_rules(letters: list[str], word):
+def check_rules(letters: list[str], word) -> bool:
+    """checks whether word is legit due to rules of target game
+
+    Args:
+        letters (list[str]): letters of grid
+        word ([type]): word to check for correctness
+
+    Returns:
+        bool: correctness of wor according to rules
+    """
     correct = False
     if len(word) >= 4 and letters[4] in word.lower():
         correct = True
@@ -56,8 +80,15 @@ def check_rules(letters: list[str], word):
 
 
 def get_words(f: str, letters: list[str]) -> list[str]:
-    """
-    Reads the file f. Checks the words with rules and returns a list of words.
+    """gets all words from file(1 word per line) that
+    are legit
+
+    Args:
+        f (str): file to get words from
+        letters (list[str]): letters of grid
+
+    Returns:
+        list[str]: list of words that are with rules
     """
     legal_words = []
     with open(f, "r") as dictionary:
@@ -68,9 +99,10 @@ def get_words(f: str, letters: list[str]) -> list[str]:
 
 
 def get_user_words() -> list[str]:
-    """
-    Gets words from user input and returns a list with these words.
-    Usage: enter a word or press ctrl+d to finish.
+    """gets user words(1 per enter) until '' not entered
+
+    Returns:
+        list[str]: list of words from user
     """
     user_words = []
     word = "hi"
@@ -84,11 +116,16 @@ def get_user_words() -> list[str]:
 def get_pure_user_words(
     user_words: list[str], letters: list[str], words_from_dict: list[str]
 ) -> list[str]:
-    """
-    (list, list, list) -> list
-
-    Checks user words with the rules and returns list of those words
+    """Checks user words with the rules and returns list of those words
     that are not in dictionary.
+
+    Args:
+        user_words (list[str]): words from user
+        letters (list[str]): letters of grid
+        words_from_dict (list[str]): words which legit from dictionary
+
+    Returns:
+        list[str]: words that are with rules but not in dictinary
     """
     pure_user_words = []
     for word in user_words:
@@ -97,19 +134,32 @@ def get_pure_user_words(
     return pure_user_words
 
 
-def results(words, legal_words, pure_words, file):
+def results(
+    words: list[str], legal_words: list[str], pure_words: list[str], file: str
+) -> str:
+    """prints result of game to user and saves result in results.txt
+
+    Args:
+        words (list[str]): words from user
+        legal_words (list[str]): words from dictionary that are legit
+        pure_words (list[str]): words that legit to rules but not in dictionary
+        file (str): file to save results to(will be created if don't exist)
+
+    Returns:
+        str: 5-line of results in results.txt
+    """
     score = 0
     for word in words:
         if word in legal_words:
             score += 1
-    print('Your score:' + str(score))
-    print('Legal words:\n',legal_words)
+    print("Your score:" + str(score))
+    print("Legal words:\n", legal_words)
     missed_words = []
     for word in legal_words:
         if word not in words:
             missed_words.append(word)
-    print('You missed:\n',missed_words)
-    print('Some mistakes:\n',pure_words)
+    print("You missed:\n", missed_words)
+    print("Some mistakes:\n", pure_words)
     with open(file, "a") as res:
         res.write(str(score) + "\n")
         res.write(", ".join(legal_words) + "\n")
@@ -118,21 +168,18 @@ def results(words, legal_words, pure_words, file):
         res.write("=====" + "\n")
 
 
-def main():
+def main() -> None:
+    """maintains a full process of game"""
     grid = generate_grid()
     display_grid(grid)
-    letters = [grid[i][j].lower() for i in range(len(grid)) for j in range(len(grid[i]))]
-    legal_words = get_words('en',letters)
+    letters = [
+        grid[i][j].lower() for i in range(len(grid)) for j in range(len(grid[i]))
+    ]
+    legal_words = get_words("en", letters)
     user_words = get_user_words()
-    pure_words = get_pure_user_words(user_words,letters,legal_words)
-    results(user_words,legal_words,pure_words,"results.txt")
+    pure_words = get_pure_user_words(user_words, letters, legal_words)
+    results(user_words, legal_words, pure_words, "results.txt")
 
 
 if __name__ == "__main__":
-    # print(get_words("en", ["e", "t", "x", "p", "h", "z", "o", "l", "i"]))
-    # print(get_user_words())
-    # display_grid(generate_grid())
-    # print(count_appearances('Conjection'))
-    # print(check_rules(["e", "t", "x", "p", "h", "z", "o", "l", "i"],"Helio"))
-    # results(["hello", "mine", "hihg"], ["hello", "high"], ["hihg"], "results.txt")
     main()
